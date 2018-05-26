7ec0087d342f202066b8e7599f4501fc4f592504
Amy Graff <amygraff.psychemath@gmail.com>
2018-05-26 15:43:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/amy/myplayground/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.amy.myplayground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonGoogle, buttonHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
